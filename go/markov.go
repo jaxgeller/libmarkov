a702fb55fbd88d6b8e526e57dd3f2dbc23091725
jaxgeller <jacksongeller@gmail.com>
2015-08-16 17:09:27 -0400

    working on golang implementation

---
+++ go/markov.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Chain struct {
+	chain map[string]string
+}
+
+func main() {
+	file, err := os.Open("test/lorem.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	buff := bufio.NewScanner(file)
+	buff.Split(bufio.ScanWords)
+
+	var words []string
+
+	dict := make(map[string][]string)
+
+	for buff.Scan() {
+		words = append(words, buff.Text())
+	}
+
+	for i := 0; i < len(words); i++ {
+		if i < len(words)-2 {
+			key := words[i] + " " + words[i+1]
+			val := words[i+2]
+
+			dict[key] = append(dict[key], val)
+		}
+	}
+}
