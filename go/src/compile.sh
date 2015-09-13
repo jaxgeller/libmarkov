fc891e8d82c29b80f6ebf4bc51fd271b9e7527ce
jaxgeller <jacksongeller@gmail.com>
2015-09-13 14:43:47 -0400

    add new binaries

--- go/src/compile.sh
+++ go/src/compile.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+rm -r ../bin
+
 for GOOS in darwin linux windows; do
   for GOARCH in 386 amd64; do
     export GOOS=$GOOS
     export GOARCH=$GOARCH
-    gb build all 
+    gb build all
   done
 done
+
+rm ../bin/libmarkov.go
