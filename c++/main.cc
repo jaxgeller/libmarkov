a06bfcadd40c5ebc07aac6b858baaa8b21a436f9
jaxgeller <jacksongeller@gmail.com>
2015-10-09 12:10:59 -0400

    add get initkey

--- c++/main.cc
+++ c++/main.cc
@@ -1,24 +1,23 @@
-// -std=c++11 -Wall
 #include <iostream>
 #include <string>
 #include <vector>
 #include <stdio.h>
 #include <string.h>
 #include <unordered_map>
+#include <ctype.h>
+
+using namespace std;
 
 int main () {
-  using namespace std;
-
-  unordered_map< string, vector<string> > map;
+  unordered_map<string, vector<string> > map;
   vector<string> holder(2);
   string prefix;
 
 
-  char str[] ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis facere doloribus possimus. Mollitia eveniet, est, praesentium illum magnam, optio itaque quae impedit reprehenderit aliquam, expedita esse voluptatum animi nesciunt porro.";
+  char str[] ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis facere doloribus possimus. Mollitia eveniet, est, praesentium illum magnam, optio itaque quae impedit reprehenderit aliquam, expedita esse voluptatum animi nesciunt porro. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque dolorem ex, reprehenderit magnam dolorum quas nisi, nostrum ipsum aliquid vitae officia fugiat! Deserunt at aliquam, vitae architecto? Iusto, accusamus, qui.";
 
   char *word;
   word = strtok (str," ");
-
 
   while (word != NULL) {
 
@@ -27,8 +26,17 @@ int main () {
     prefix = holder[0] + " " + holder[1];
 
     map[prefix].push_back(word);
-
     word = strtok (NULL, " ");
+  }
+
+
+  for (auto m = map.begin(); m != map.end(); ++m) {
+
+    if ( isupper(m->first[0]) ) {
+      cout << m->first << "\n";
+      break;
+    }
+
   }
 
   return 0;
